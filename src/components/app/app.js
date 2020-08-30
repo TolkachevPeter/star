0487252aaab41c99002b946b257f3b040a1040d9
TolkachevPeter <peter.tolkachev@gmail.com>
2020-08-30 18:51:45 +0300

    changed swapi-service

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,13 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import SwapiService from '../../services/swapi-service';
 
 import './app.css';
 import PeoplePage from '../people-page/people-page';
 
 export default class App extends Component {
-
+  swapiService = new SwapiService();
   state = {
     showRandomPlanet: true,
   };
@@ -41,8 +42,31 @@ export default class App extends Component {
         </button>
       </div>
       <PeoplePage/>
-      <PeoplePage/>
-      <PeoplePage/>
+
+      <div className="row mb2">
+      <div className="col-md-6">
+        <ItemList 
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPlanets}
+        />
+      </div>
+      <div className="col-md-6">
+        <PersonDetails personId={this.state.selectedPerson} />
+      </div>
+    </div>
+
+    <div className="row mb2">
+      <div className="col-md-6">
+        <ItemList 
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllStarships}
+        />
+      </div>
+      <div className="col-md-6">
+        <PersonDetails personId={this.state.selectedPerson} />
+      </div>
+    </div>
+
       </div>
 
 
