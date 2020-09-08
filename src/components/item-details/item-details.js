22c87de6f8b2ee580c8ebbaa41c229a3a096c1c8
Tolkachev Peter <peter.tolkachev@gmail.com>
2020-09-08 23:23:46 +0300

    all is broken

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
-import './item-details.css';
+import ErrorButton from '../error-button/error-button';
 import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner';
+
+import './item-details.css';
+
+const Record = ({ item, field, label }) => {
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{ item[field] }</span>
+    </li>
+  );
+};
+
+export {
+  Record
+};
+
 
 export default class ItemDetails extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    item: null 
+    item: null,
+    image: null
   };
 
   componentDidMount() {
@@ -17,61 +33,54 @@ export default class ItemDetails extends Component {
   }
 
   componentDidUpdate(prevProps) {
-if(this.props.itemId !== prevProps.itemId) {
-  this.updatePerson();
-}
+    if (this.props.itemId !== prevProps.itemId) {
+      this.updateItem();
+    }
   }
 
-  updatePerson() {
-    const { itemId } = this.props;
+  updateItem() {
+    const { itemId, getData, getImageUrl } = this.props;
     if (!itemId) {
       return;
     }
 
-    this.swapiService
-    .getPerson(itemId)
-    .then((item) => {
-      this.setState({ item });
-    }) 
+    getData(itemId)
+      .then((item) => {
+        this.setState({
+          item,
+          image: getImageUrl(item)
+        });
+      });
   }
 
   render() {
 
-    if(!this.state.item) {
+    const { item, image } = this.state;
+    if (!item) {
       return <span>Select a item from a list</span>;
     }
 
-    if(!this.state.item){
-      return <Spinner />
-    }
-
-    const {
-      id, name, gender,
-      birthYear, eyeColor} = this.state.person;
+    const { id, name, gender,
+              birthYear, eyeColor } = item;
 
     return (
-      <div className="person-details card">
-        <img className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+      <div className="item-details card">
+        <img className="item-image"
+          src={image}
+          alt="item"/>
 
         <div className="card-body">
           <h4>{name}</h4>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender} </span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
+            {
+              React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, { item });
+              })
+            }
           </ul>
+          <ErrorButton />
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,59 +1,20 @@
 import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../item-details/item-details';
-import ErrorIndicator from '../error-indicator/error-indicator';
+import ItemDetails from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
+import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 
 import './people-page.css';
 
-
-const Row = ({ left , right }) => {
-  return (
-    <div className="row mb2">
-    <div className="col-md-6">
-      {left}
-    </div>
-    <div className="col-md-6">
-      {right}
-    </div>
-  </div>
-  )
-}
-
-class ErrorBoundry extends Component {
-
-  state = {
-    hasError: false
-  };
-
-  componentDidCatch(){
-    this.setState({
-      hasError: true
-    })
-  }
-
-  render(){
-    if (this.state.hasError) {
-      return <ErrorIndicator />
-    }
-    return this.props.chilndren;
-  }
-
-}
 export default class PeoplePage extends Component {
+
   swapiService = new SwapiService();
 
-  state ={
-    selectedPerson: 3,
-    hasError: false
-  }
-
-  componentDidCatch(){
-    this.setState({
-      hasError: true
-    })
-  }
+  state = {
+    selectedPerson: 11
+  };
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
@@ -61,31 +22,26 @@ export default class PeoplePage extends Component {
 
   render() {
 
-    if (this.state.hasError) {
-      return <ErrorIndicator />
-    }
-
     const itemList = (
-      <ItemList 
-      onItemSelected={this.onPersonSelected}
-      getData={this.swapiService.getAllPeople}>
-     {(i) => (
-    `${i.name}`
-    )}
-     </ItemList>
-    )
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}>
+
+        {(i) => (
+          `${i.name} (${i.birthYear})`
+        )}
+
+      </ItemList>
+    );
 
     const personDetails = (
-    <PersonDetails personId={this.state.selectedPerson} />
-    )
-
+      <ErrorBoundry>
+        <ItemDetails itemId={this.state.selectedPerson} />
+      </ErrorBoundry>
+    );
 
     return (
-      
       <Row left={itemList} right={personDetails} />
-   
-    )
-
+    );
   }
-  
 }
