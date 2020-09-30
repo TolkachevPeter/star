9883afce6f4c7f313dd457be32269032250b48a0
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-01 00:06:57 +0300

    added React.Router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,6 +15,8 @@ import {
 
 import './app.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 export default class App extends Component {
 
@@ -28,13 +30,15 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.swapiService} >
+          <Router>
         <div className="stardb-app">
         <Header />
         <RandomPlanet/>
-        <PeoplePage/>
-        <StarshipPage/>
-        <PlanetPage/>
+          <Route path='/people' component={PeoplePage} />
+          <Route path='/planets' component={PlanetPage} />
+          <Route path='/starships' component={StarshipPage} />
         </div>
+        </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
